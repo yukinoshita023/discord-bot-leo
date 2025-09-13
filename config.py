1cfdf95379d7d2611cbfc5008eddbd47c29a1786
taisei023 <tm.lovin8@gmail.com>
2025-09-13 12:56:24 +0900

    #3 レオ仕様に変更2

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
-VOICE_CHANNEL_ID = 847514073964740679
+VOICE_CHANNEL_ID = 847158182257754116
 
 # 女性の場合: ja-JP-NanamiNeural, 男性の場合: ja-JP-KeitaNeural
 VOICE_MODEL = "ja-JP-KeitaNeural"
